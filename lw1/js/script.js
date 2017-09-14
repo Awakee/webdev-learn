07fe562a7d9e8cb99ba4f89f54bb6d9b4e6f863b
Awakee <screamm97@gmail.com>
2017-09-14 18:55:21 +0300

    Fix issuess #1

--- lw1/js/script.js
+++ lw1/js/script.js
@@ -15,23 +15,21 @@ alert(demoShowarrNumbers);
 
 var enterNumber = prompt("Введите число для поиска в массиве");
 
-if(isNaN(enterNumber)) {
-  alert("Ошибка, повторите ввод");
-  windows.stop()
+function checkEnter(numberCheck) {
+  if((numberCheck == "") || (isNaN(numberCheck))) {
+    alert("Повторите ввод");
+    windows.stop();
+  }
 }
 
-if((enterNumber == "")) {
-  alert("Ошибка, заполните поле.");
-  windows.stop()
-}
-
+checkEnter(enterNumber);
 alert("Введеное число: " + enterNumber);
 
 var searchElement = arrNumbers.filter(function(number) {
   return number == enterNumber;
 });
 
-if (searchElement == false) {
+if (!searchElement) {
   alert("Элемент не найден");
 } else {
   alert("Элемент найден. " + searchElement);
