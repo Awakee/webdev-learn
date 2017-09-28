1efe0ddc5bf7a752615640eb65a07a791555928f
Awakee <screamm97@gmail.com>
2017-09-29 01:54:43 +0300

    Fix checking before validation #3

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -4,26 +4,31 @@ function registration(){
   var userPassCheck = document.getElementById('password_repeat');
   var userAgreement = document.getElementById('check_agreement');  
   var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail.value));
-    
-  if ((userEmail.value.length < 5) || (!emailCheck)) {
-    alert("Ошибка ввода Email.");
-    windows.stop();
-  }
 
-  if ((userPass.value.length < 6) || (userPass == "") || (userPassCheck == "")) {
-    alert("Ошибка ввода пароля");
+  if ((userEmail.value == "") || (userPass.value == "") || (userPassCheck.value == "")){
+    alert("Ошибка ввода."); 
     windows.stop();
-  }
+  }else{
+    if ((userEmail.value.length < 5) || (!emailCheck)) {
+        alert("Ошибка ввода Email.");
+        windows.stop();
+    }
 
-  if ((userPass.value != userPassCheck.value)) {
-    alert("Пароли должны совпадать");
-    windows.stop();
-  }
+    if ((userPass.value.length < 6) || (userPass == "") || (userPassCheck == "")) {
+        alert("Ошибка ввода пароля");
+        windows.stop();
+    }
 
-  if (!userAgreement.checked){
-    alert("Вы должны принять пользовательское соглашение");
-    windows.stop();
-  }
+    if ((userPass.value != userPassCheck.value)) {
+        alert("Пароли должны совпадать");
+        windows.stop();
+    }
 
-  alert("Регистрация успешна");
+    if (!userAgreement.checked){
+        alert("Вы должны принять пользовательское соглашение");
+        windows.stop();
+    }
+
+    alert("Регистрация успешна");
+    }
 };
