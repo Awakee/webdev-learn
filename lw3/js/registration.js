a876856e3140e2b006cc6cce14cd2ad49503b2dd
Awakee <screamm97@gmail.com>
2017-09-29 01:23:45 +0300

    Fix some issues #3

---
+++ lw3/js/registration.js
@@ -0,0 +1,29 @@
+function registration(){
+    var userEmail = document.getElementById('email');
+    var userPass = document.getElementById('password');
+    var userPassCheck = document.getElementById('password_repeat');
+    var userAgreement = document.getElementById('check_agreement');  
+    var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail.value));
+    
+    if ((userEmail.value.length < 5) || (!emailCheck)) {
+        alert("Ошибка ввода Email.");
+        windows.stop();
+    }
+
+    if ((userPass.value.length < 6) || (userPass == "") || (userPassCheck == "")) {
+        alert("Ошибка ввода пароля");
+        windows.stop();
+    }
+
+    if (!(userPass.value == userPassCheck.value)) {
+       alert("Пароли должны совпадать");
+       windows.stop();
+    }
+
+    if (!userAgreement.checked){
+        alert("Вы должны принять пользовательское соглашение");
+        windows.stop();
+    }
+
+    alert("Регистрация успешна");
+};
