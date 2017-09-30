7904e08c7b89f00fab462e86428cccc742515f13
Awakee <screamm97@gmail.com>
2017-09-30 15:52:29 +0300

    Upload new ver.

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,34 +1,39 @@
-function registration(){
+function registration(event){
   var userEmail = document.getElementById('email');
   var userPass = document.getElementById('password');
   var userPassCheck = document.getElementById('password_repeat');
   var userAgreement = document.getElementById('check_agreement');  
   var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail.value));
 
+  event.preventDefault();
   if ((userEmail.value == "") || (userPass.value == "") || (userPassCheck.value == "")){
     alert("Ошибка ввода."); 
-    windows.stop();
+    return;
   }else{
     if ((userEmail.value.length < 5) || (!emailCheck)) {
         alert("Ошибка ввода Email.");
-        windows.stop();
+        return;
     }
 
     if ((userPass.value.length < 6) || (userPass == "") || (userPassCheck == "")) {
         alert("Ошибка ввода пароля");
-        windows.stop();
+        return;
     }
 
-    if ((userPass.value != userPassCheck.value)) {
+    if (userPass.value != userPassCheck.value) {
         alert("Пароли должны совпадать");
-        windows.stop();
+        return;
     }
 
     if (!userAgreement.checked){
         alert("Вы должны принять пользовательское соглашение");
-        windows.stop();
+        return;
     }
 
     alert("Регистрация успешна");
     }
+}
+
+window.onload = function(){
+  document.getElementById("registration-form").addEventListener("submit", registration);
 };
